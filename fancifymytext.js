533a8ec26881fabb7d8b00c1f7d96d0b25b599a2
LeaithR <112685563+LeaithR@users.noreply.github.com>
2024-04-01 14:07:49 -0700

    alertbox

---
+++ fancifymytext.js
@@ -0,0 +1,7 @@
+const buttonAlert = document.querySelector('#Bigger');
+
+buttonAlert.addEventListener("click" ,() => {
+    alert("Hello, world!");
+})
+
+//<script>alert("Hello, world!");</script>
